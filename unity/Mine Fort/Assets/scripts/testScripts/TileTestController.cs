146c54208d596c544db0440ed8ad94e5cf4472d7
Cristiano Ferreira <cristianorj@gmail.com>
2017-07-14 16:25:16 -0300

    movendo o projeto vs para cá, de novo

--- unity/Mine Fort/Assets/scripts/testScripts/TileTestController.cs
+++ unity/Mine Fort/Assets/scripts/testScripts/TileTestController.cs
@@ -26,7 +26,7 @@ public class TileTestController : MonoBehaviour {
 
                 SpriteRenderer tile_sr = tile_go.AddComponent<SpriteRenderer>();
                 Tile tile_data = world.GetTileAt(x, y);
-                tile_go.transform.position = new Vector3(x / 2f, y / 2f, y/10f);
+                tile_go.transform.position = CalcPosition(tile_go,x, y,45);
                 if (tile_data.Type == Rimworld.model.GameConsts.TileType.Floor)
                 {
                     tile_sr.sprite = floorSprite;
@@ -38,6 +38,38 @@ public class TileTestController : MonoBehaviour {
             }
         }
 	}
+
+    /*
+     function isoTo2D(pt:Point):Point{
+  var tempPt:Point = new Point(0, 0);
+  tempPt.x = (2 * pt.y + pt.x) / 2;
+  tempPt.y = (2 * pt.y - pt.x) / 2;
+  return(tempPt);
+}
+         function twoDToIso(pt:Point):Point{
+  var tempPt:Point = new Point(0,0);
+  tempPt.x = pt.x - pt.y;
+  tempPt.y = (pt.x + pt.y) / 2;
+  return(tempPt);
+}
+         */
+
+
+
+    private Vector3 CalcPosition(GameObject tile_go,float x, float y,float angle)
+    {
+        /*   var eulers = tile_go.transform.rotation.eulerAngles;
+           //tile_go.transform.rotation = Quaternion.Euler(angle, eulers.y, eulers.z);
+
+           Vector3 pos=new Vector3(x/2f , y/2f , 0);
+           pos=Quaternion.AngleAxis(30, Vector3.up) * pos;
+           pos = Quaternion.AngleAxis(45, Vector3.left) * pos;
+           return pos;*/
+        x = x / 2f;
+        y = y / 2f;
+        Vector3 pos = new Vector3(x-y,(x+y)/2, -y/2f);
+        return pos;
+    }
 
     private void RandomizeTiles(World world)
     {
